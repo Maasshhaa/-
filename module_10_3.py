3c1007e40d98b9277b3649b73dd38ecef789563b
Мария <mariyagolcova2004@gmail.com>
2024-11-05 18:45:23 +0300

    Add files via upload

--- module_10_3.py
+++ module_10_3.py
@@ -1,36 +1,35 @@
 import threading
 from random import randint
 import time
-from time import sleep
 
 
 class Bank:
-    lock = threading.Lock()
-    def __init__(self, balance = 0):
+    def __init__(self, balance=0):
         self.balance = int(balance)
-
+        self.lock = threading.Lock()
 
     def deposit(self):
         for i in range(100):
             time.sleep(0.001)
             rand = randint(50, 500)
-            self.balance += rand
-            print(f'Пополнение: {rand}. Баланс: {self.balance}')
-            if self.balance >= 500 and self.lock.locked():
-                self.lock.release()
+            with self.lock:
+                if self.balance + rand > 500:
+                    pass
+                else:
+                    self.balance += rand
+                    print(f'Пополнение: {rand}. Баланс: {self.balance}')
 
     def take(self):
         for i in range(100):
             time.sleep(0.001)
             rand = randint(50, 500)
-            print(f'Запрос на {rand}, при текущем балансе - {self.balance}')
-            if self.balance - rand >= 0:
-                self.balance -= rand
-                print(f'Снятие: {rand}. Баланс: {self.balance}')
-            else:
-                print(f'Запрос отклонён, недостаточно средств')
-                self.lock.acquire()
-
+            with self.lock:
+                print(f'Запрос на {rand}, при текущем балансе - {self.balance}')
+                if self.balance - rand >= 0:
+                    self.balance -= rand
+                    print(f'Снятие: {rand}. Баланс: {self.balance}')
+                else:
+                    print(f'Запрос отклонён, недостаточно средств')
 
 bk = Bank()
 th1 = threading.Thread(target=Bank.deposit, args=(bk,))
@@ -40,5 +39,4 @@ th1.start()
 th2.start()
 th1.join()
 th2.join()
-
 print(f'Итоговый баланс: {bk.balance}')
