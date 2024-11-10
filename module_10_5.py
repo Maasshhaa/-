b09628be6968242a6e244a8f6d6e3280fc2adf73
Мария <mariyagolcova2004@gmail.com>
2024-11-10 21:38:10 +0300

    Многопроцессное программирование

---
+++ module_10_5.py
@@ -0,0 +1,31 @@
+import time
+from multiprocessing import Pool
+
+
+def read_info(name):
+    all_data = []
+    file = open(name, 'r')
+    while True:
+        line = file.readline()
+        all_data.append(line)
+        if not line:
+            break
+    file.close()
+
+filenames = [f'./file {number}.txt' for number in range(1, 5)]
+
+
+# stared_at = time.time()
+# result = list(map(read_info, filenames))
+# ended_at = time.time()
+# elapsed = ended_at - stared_at
+# print(f'Время на выполнении функции последовательно - {elapsed}') #6.56002950668335
+
+if __name__ == '__main__':
+    stared_at = time.time()
+    with Pool() as pool:
+        results = pool.map(read_info, filenames)
+    ended_at = time.time()
+    elapsed = ended_at - stared_at
+    print(f'Время на выполнении функции многопроцессорно - {elapsed}') #2.6709513664245605
+
