546fb29f775567173f924e1c38c925b86105dfaa
Мария <mariyagolcova2004@gmail.com>
2025-01-28 20:14:12 +0300

    FastAPI начало

---
+++ module_16_1.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from fastapi import Query
+
+app = FastAPI()
+
+@app.get("/")
+async def read_root():
+    return {"message": "Главная страница"}
+
+@app.get("/user/admin")
+async def read_admin():
+    return {"message": "Вы вошли как администратор"}
+
+@app.get("/user/{user_id}")
+async def read_user(user_id: int):
+    return {"message": f"Вы вошли как пользователь № {user_id}"}
+
+@app.get("/user")
+async def read_user_info(username: str = Query(...), age: int = Query(...)):
+    return {"message": f"Информация о пользователе. Имя: {username}, Возраст: {age}"}
